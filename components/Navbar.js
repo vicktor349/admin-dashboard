ab74601b8a95b9ca6b3e99049e0a318693c8602c
vicktoh <ayomidevictor349@gmail.com>
2024-02-01 15:50:10 +0100

    Navbar inclusion

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,33 @@
+import Image from 'next/image'
 import React from 'react'
+import SearchBar from './Search';
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { Avatar } from '@mui/material';
 
 const Navbar = () =>
 {
     return (
-        <div>Navbar</div>
+        <div className="flex align-middle">
+            <div>
+                <Image src={"/images/logo.svg"} alt='logo' width={50} height={50} />
+            </div>
+            <h3>Dashboard</h3>
+            <div>
+                <SearchBar />
+            </div>
+            <div>
+                <IoIosNotificationsOutline />
+                <div>
+                    <Avatar src="/images/avatar.jpg" alt="Avatar Image" />
+                    <div>
+                        <p>Segun Layomi</p>
+                        <p>ayomidevictor349@gmail.com</p>
+                    </div>
+                    <MdKeyboardArrowDown />
+                </div>
+            </div>
+        </div>
     )
 }
 
---
+++ components/Search.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { CiSearch } from "react-icons/ci";
+
+const SearchBar = () =>
+{
+    return (
+        <div className="flex items-center border border-gray-300 p-2 rounded">
+            <span className="text-gray-500 mr-2">
+                <CiSearch />
+            </span>
+            <input
+                type="text"
+                placeholder="Search..."
+                className="outline-none focus:outline-none w-full"
+            />
+        </div>
+    );
+};
+
+export default SearchBar;
