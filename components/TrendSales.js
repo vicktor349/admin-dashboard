69739a5d880dbaafbed5e5dc9c01a60c3d0f204f
vicktoh <ayomidevictor349@gmail.com>
2024-02-09 11:28:03 +0100

    Adjusted height and width

--- components/TrendSales.js
+++ components/TrendSales.js
@@ -101,8 +101,8 @@ const TrendSales = () =>
     }
 
     return (
-        <div className="w-[50rem]">
-            <div className='border border-borderColor pt-0 rounded-xl shadow-xl w-full'>
+        <div className="w-[53.5rem]">
+            <div className='border border-borderColor pt-0 rounded-xl shadow-xl w-full h-[33.7rem]'>
                 <div className='flex mx-6 items-center'>
                     <h1 className="text-xl py-6">Sales Trend</h1>
                     <div className="ml-auto">
