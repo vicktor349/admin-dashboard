79f29bcb1bea52181c4b6b8e732ff8c92485963b
vicktoh <ayomidevictor349@gmail.com>
2024-02-09 13:33:47 +0100

    sales statistics

---
+++ components/Total.js
@@ -0,0 +1,45 @@
+import Image from 'next/image'
+import React from 'react'
+import { AreaChart, Area, ResponsiveContainer } from 'recharts';
+
+const Total = ({ src, alt, data, stroke, fill, total, totalNumber, profits, backgroundColor, textColor }) =>
+{
+    return (
+        <div className='rounded-xl border shadow-lg w-72 h-64 px-4 pt-4'>
+            <div className='flex items-center'>
+                <Image src={src} alt={alt} width={50} height={50} />
+                <ResponsiveContainer width="50%" height={50} className="ml-auto">
+                    <AreaChart
+                        width={10}
+                        height={50}
+                        data={data}
+                        syncId="anyId"
+                        margin={{
+                            top: 10,
+                            right: 30,
+                            left: 0,
+                            bottom: 0,
+                        }}
+                    >
+                        <Area type="monotone" dataKey="pv" stroke={stroke} fill={fill} />
+                    </AreaChart>
+                </ResponsiveContainer>
+            </div>
+            <div className='mt-2'>
+                <p className='text-lg py-2 text-[#898989]'>{total}</p>
+                <p className='font-semibold text-xl py-2'>{totalNumber}</p>
+            </div>
+            <div>
+                <div className='flex items-center mt-10'>
+                    <div className={`flex items-center ${backgroundColor} ${textColor} py-1 px-2 rounded-3xl`}>
+                        <Image src={"/images/graph.svg"} alt="graph" width={20} height={20} />
+                        <span className='text-sm ml-2'>{profits}</span>
+                    </div>
+                    <span className=' text-[11px] ml-2'>vs. previous month</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Total
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import TrendSales from "@/components/TrendSales";
 import SideNavbar from "@/components/SideNavbar";
 import Head from "next/head";
+import Total from "@/components/Total";
+import { TotalOrderData, TotalRefundData } from "@/data/data";
+
+
 export default function Home()
 {
   return (
@@ -8,12 +12,47 @@ export default function Home()
       <Head>
         <title>Admin | Dashboard</title>
       </Head>
-      <main className="font-Poppins grid grid-cols-28">
-        <div className="col-start-1 col-end-2">
+      <main className="font-Poppins flex">
+        <div>
           <SideNavbar />
         </div>
-        <div className="col-start-2 col-end-8 mt-5">
+        <div className="mt-5 ml-24">
           <TrendSales />
+        </div>
+        <div className="mt-5 ml-6 grid grid-cols-2 gap-6">
+          <Total src={"images/totalorder.svg"}
+            alt="refund chart" data={TotalOrderData}
+            stroke="#82ca9d" fill="#82ca9d"
+            total="Total Order" totalNumber="350"
+            profits="37.50%"
+            backgroundColor="bg-[#34caa5]/10"
+            textColor="text-[#34caa5]"
+          />
+          <Total src={"images/totalrefund.svg"}
+            alt="refund chart" data={TotalRefundData}
+            stroke="#ED544E" fill="#ED544E"
+            total="Total Refund" totalNumber="270"
+            profits="-33.33%"
+            backgroundColor="bg-[#ef716c]/10"
+            textColor="text-[#ef716c]"
+          />
+          <Total src={"images/averagesales.svg"}
+            alt="refund chart" data={TotalOrderData}
+            stroke="#EF716C" fill="#EF716C"
+            total="Average Sales" totalNumber="1567"
+            profits="-17.65%"
+            backgroundColor="bg-[#ef716c]/10"
+            textColor="text-[#ef716c]"
+            fillColor="text-[#ef716c]"
+          />
+          <Total src={"images/totalincome.svg"}
+            alt="refund chart" data={TotalOrderData}
+            stroke="#82ca9d" fill="#82ca9d"
+            total="Total Income" totalNumber="$350,000"
+            profits="62.50%"
+            backgroundColor="bg-[#34caa5]/10"
+            textColor="text-[#34caa5]"
+          />
         </div>
       </main>
     </div>
